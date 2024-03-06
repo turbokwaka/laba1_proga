d2ac3bef01271e0f2a57deb5bd55fa3639a3302e
artem <taranenko.artem@lll.kpi.ua>
2024-03-06 10:48:35 +0200

    modified smth

--- ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+159b5d1c33f1704386e2efb0f11cfa0853de3bd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
